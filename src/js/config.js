53f998d3e5b52847cde518881e5c6791edad7769
Adiba111 <adibaxujayeva1998@gmail.com>
2023-10-10 08:32:12 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e2d460f8-d44c-4b52-8f57-3c19ec78c743';
+export const MODAL_CLOSE_SEC = 2.5;
